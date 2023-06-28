af0af269b4dc3e1cee2ad59d624f2a576f25e47f
andrade-felipe-dev <83104193+andrade-felipe-dev@users.noreply.github.com>
2023-06-28 15:14:14 -0300

    commit
    Commit atualizado

--- desafio02/node/index.js
+++ desafio02/node/index.js
@@ -5,15 +5,18 @@ const config = {
     host: 'db',
     user: 'root',
     password: 'root',
-    port: '3306',
+    port: 3306,
     database: 'desafio02db'
 };
 
-const mysql = require('mysql')
+const mysql = require('mysql2')
 const connection = mysql.createConnection(config)
+
+const createTable = `CREATE TABLE people (id INT AUTO_INCREMENT PRIMARY KEY, nome VARCHAR(255))`
 
 const sql = `INSERT INTO people(name) values('Felipe')`
 const peoples = `SELECT * FROM people;`
+connection.query(createTable)
 connection.query(sql)
 connection.end()
 
