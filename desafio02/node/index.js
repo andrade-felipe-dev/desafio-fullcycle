7fe665453e4b7b19530a2e1b723a6c9e98b15bc2
andrade-felipe-dev <83104193+andrade-felipe-dev@users.noreply.github.com>
2023-06-28 16:17:56 -0300

    commit atualizado
    Commit atualizado

--- desafio02/node/index.js
+++ desafio02/node/index.js
@@ -1,30 +1,51 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 const config = {
-    host: 'db',
-    user: 'root',
-    password: 'root',
-    port: 3306,
-    database: 'desafio02db'
+  host: 'db',
+  user: 'root',
+  password: 'root',
+  database: 'desafio02db'
 };
 
-const mysql = require('mysql2')
-const connection = mysql.createConnection(config)
+const mysql = require('mysql2');
+const connection = mysql.createConnection(config);
 
-const createTable = `CREATE TABLE people (id INT AUTO_INCREMENT PRIMARY KEY, nome VARCHAR(255))`
+const createTable = `CREATE TABLE IF NOT EXISTS people (id INT AUTO_INCREMENT PRIMARY KEY, nome VARCHAR(255))`;
 
-const sql = `INSERT INTO people(name) values('Felipe')`
-const peoples = `SELECT * FROM people;`
-connection.query(createTable)
-connection.query(sql)
-connection.end()
+const insertQuery = `INSERT INTO people(nome) VALUES('Felipe')`;
 
-app.get('/', (req, res) => {
-    res.send('<h1> Full Cycle </h1>')
-    res.send(`<h2> ${peoples} </h2>`)
-})
+const selectQuery = `SELECT * FROM people`;
 
-app.listen(port, () => {
-    console.log('rodando na porta ' + port)
-})
+connection.query(createTable, (error) => {
+  if (error) {
+    console.error('Erro ao criar a tabela:', error);
+    connection.end();
+    return;
+  }
+
+  connection.query(insertQuery, (error) => {
+    if (error) {
+      console.error('Erro ao inserir dados:', error);
+      connection.end();
+      return;
+    }
+
+    connection.query(selectQuery, (error, results) => {
+      if (error) {
+        console.error('Erro ao executar a consulta:', error);
+        connection.end();
+        return;
+      }
+
+      const result = results;
+      connection.end();
+      app.get('/', (req, res) => {
+        res.send(`<h1>Full Cycle</h1><h2>${JSON.stringify(result)}</h2>`);
+      });
+      app.listen(port, () => {
+        console.log('Rodando na porta ' + port);
+      });
+    });
+  });
+});
